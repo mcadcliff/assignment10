f72a03465c214e860f3186acae9fe23612c0d82a
mcadcliff <44850474+mcadcliff@users.noreply.github.com>
2019-04-07 21:28:06 -0500

    initial commit

---
+++ assignment 10/script.js
@@ -0,0 +1,38 @@
+var myVue = new Vue({
+    el: "#app",
+    data: {
+        console: [
+            
+            {name: "King Cobra",
+            type: "bottle cap",
+            location: "Missouri",
+            year: "1854",
+            imgSrc: "kingcobra.jpg"},
+    
+            
+            {name: "Finnegans Brew Co.",
+            type: "bottle cap",
+            location: "Minnesota",
+            year: "2000",
+            imgSrc: "finnegans.jpg"},
+            
+            {name:"I don't know",
+            type: "bottle cap",
+            location: "The Ground",
+            year: "somethin somethin",
+            imgSrc: "idk.jpg"},
+            
+            {name: "Full Sail Brewing Co.",
+            type: "bottle cap",
+            location: "Oregon",
+            year: "1987",
+            imgSrc: "fullsail.jpg"},
+            
+            {name: "Pyramid Brewing Co.",
+            type: "bottle cap",
+            location: "Washington",
+            year: "1984",
+            imgSrc: "pyramid.jpg"}
+        ]   
+    }
+});
